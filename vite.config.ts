7faa68ce2a24c39b3fe221c492bd63fdfd49d8d6
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2025-02-13 16:15:18 +0200

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Carvy/', // This should match the repository name
   plugins: [react()],
-  base: '/'
 })
